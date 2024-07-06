6893258317990b3257beb19585a0c8673ecefacc
awskumarsm2405 <awskumarsm2405@gmail.com>
2024-07-07 00:02:38 +0530

    commit

--- terraform-manifests/01-main.tf
+++ terraform-manifests/01-main.tf
@@ -14,11 +14,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "=3.0.0"
     }
-    azuread = {
+     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.0"
+      version = "~> 2.15.0"
     }
     random = {
       source  = "hashicorp/random"
